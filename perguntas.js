db7277efd608c8ea9bae46849e4f304fc716f62e
silviosnjr <silviosnjr@gmail.com>
2024-08-15 16:45:59 -0300

    Criando elemento JS no HTML

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,9 @@ criaCartao(
     'O que é uma função ?', 
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em inglês',
+    'Oi em inglês é HI (RAI)'
+)
