af9570a084776a8081c890936d6f66391929e082
silviosnjr <silviosnjr@gmail.com>
2024-09-02 14:23:36 -0300

    novas perguntas

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,84 @@
 criaCartao(
-    'Programação', 
-    'O que é Python?', 
-    'O Python é uma linguagem de programação'
+    'Ciências', 
+    'Qual é a fórmula química da água?', 
+    'A fórmula química da água é H2O'
 )
 
 criaCartao(
     'Geografia', 
-    'Qual a capital da França?', 
-    'A capital da França é Paris'
+    'Qual é o rio mais longo do mundo?', 
+    'O rio mais longo do mundo é o Rio Nilo'
 )
 
 criaCartao(
-    'Programação', 
-    'O que é uma função ?', 
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Língua Portuguesa', 
+    'O que é uma metáfora?', 
+    'Uma metáfora é uma figura de linguagem que compara duas coisas diferentes sem usar as palavras "como" ou "tal qual"'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em inglês',
-    "Oi em inglês é HI (RAI)"
+    'Atualidade', 
+    'Qual é o nome do atual presidente dos Estados Unidos?', 
+    'O atual presidente dos Estados Unidos é Joe Biden'
+)
+
+criaCartao(
+    'Esportes', 
+    'Quantas Copas do Mundo de Futebol o Brasil já venceu?', 
+    'O Brasil já venceu 5 Copas do Mundo de Futebol'
+)
+
+criaCartao(
+    'Biologia', 
+    'Qual é a molécula responsável pelo transporte de oxigênio no sangue?', 
+    'A molécula responsável pelo transporte de oxigênio no sangue é a hemoglobina'
+)
+
+criaCartao(
+    'Literatura', 
+    'Quem escreveu "Dom Casmurro"?', 
+    'O livro "Dom Casmurro" foi escrito por Machado de Assis'
+)
+
+
+criaCartao(
+    'Química', 
+    'Qual é o elemento químico com símbolo "Fe"?', 
+    'O elemento químico com símbolo "Fe" é o Ferro'
+)
+
+criaCartao(
+    'Física', 
+    'Quem formulou a Lei da Gravitação Universal?', 
+    'A Lei da Gravitação Universal foi formulada por Isaac Newton'
+)
+
+criaCartao(
+    'Geografia', 
+    'Qual é o maior país em extensão territorial?', 
+    'O maior país em extensão territorial é a Rússia'
+)
+
+criaCartao(
+    'Matemática', 
+    'Qual é o valor de π (pi) aproximadamente?', 
+    'O valor de π (pi) é aproximadamente 3,14'
+)
+
+criaCartao(
+    'Tecnologia', 
+    'O que é inteligência artificial?', 
+    'Inteligência artificial é a simulação da inteligência humana por máquinas, especialmente por sistemas de computador'
+)
+
+criaCartao(
+    'Artes', 
+    'Quem pintou "A Monalisa"?', 
+    '"A Monalisa" foi pintada por Leonardo da Vinci'
+)
+
+criaCartao(
+    'Astronomia', 
+    'Qual é o planeta mais próximo do Sol?', 
+    'O planeta mais próximo do Sol é Mercúrio'
 )
