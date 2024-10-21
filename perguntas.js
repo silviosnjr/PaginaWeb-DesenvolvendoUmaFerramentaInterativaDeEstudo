94b6f486f854b4d75f7aff63d5efa6d604686cfa
silviosnjr <silviosnjr@gmail.com>
2024-10-21 01:56:14 -0300

    arquivo perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação.'
+)
+criaCartao(
+    'Estilo',
+    'O que é CSS?',
+    'O CSS é uma lingugem de estilização.'
+)
+criaCartao(
+    'Marcação',
+    'O que é HTML?',
+    'O HTML é uma lingugem de estruturação e semântica do conteúdo.'
+)
+criaCartao(
+    'IDE',
+    'O que é VSCode?',
+    'O Visual Studio Code é um editor de código com diversas integrações.'
+)
