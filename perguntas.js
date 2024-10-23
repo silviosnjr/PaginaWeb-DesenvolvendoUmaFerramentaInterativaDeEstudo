9238052de8c9f1c15c2d7f9ee153396b7c28fa45
silviosnjr <silviosnjr@gmail.com>
2024-10-23 15:59:49 -0300

    Código fonte do mestre mandou

--- perguntas.js
+++ perguntas.js
@@ -1,53 +1,127 @@
-const personagens = [
-    'Drácula', 
-    'Frankenstein', 
-    'Fantasma', 
-    'Morcego', 
-    'Múmia', 
-    'Espantalho', 
-    'Lobsomen', 
-    'Coruja', 
-    'Bruxa', 
-    'Zumbi'
-];
+criaCartao(
+    '01', 
+    'Mestre mandou...', 
+    'Enviar uma emoção de tristeza.',
+    'sad.png',
+    'fake' /*Se fake envie fake*/ 
+)
 
-const imagens = [
-    'dracula.png', 
-    'frankenstein.png', 
-    'ghost.png', 
-    'bat.png', 
-    'mummy.png', 
-    'scarecrow.png', 
-    'werewolf.png', 
-    'wisdom.png', 
-    'witch.png', 
-    'zombie.png'
-];
+criaCartao(
+    '02', 
+    'Mestre mandou...', 
+    'Fazer coração com os dedos da mão.',
+    'hand.png',
+    ''
+)
 
-// Combina personagens e imagens em um array de pares
-let cartas = [];
+criaCartao(
+    '03', 
+    'Mestre mandou...', 
+    'Apresente um objeto na cor preta.',
+    'pool.png',
+    ''
+)
 
-for (let i = 0; i < personagens.length; i++) {
-    cartas.push({ personagem: personagens[i], imagem: imagens[i] });
-}
+criaCartao(
+    '04', 
+    'Mestre mandou...', 
+    'Bater palmas com as mãos.',
+    'clapping.png',
+    ''
+)
 
-// Duplicar cada carta para formar pares
-cartas = [...cartas, ...cartas];
+criaCartao(
+    '05', 
+    'Mestre mandou...', 
+    'Ligar o microfone e falar um elogio qualquer.',
+    'chat-bubbles.png',
+    ''
+)
 
-// Função para embaralhar o array (algoritmo de Fisher-Yates)
-function embaralharCartas(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
+criaCartao(
+    '06', 
+    'Mestre mandou...', 
+    'Ligar o microne e falar um palavrão qualquer.',
+    'swearing.png',
+    'fake'
+)
 
-// Embaralha o array de cartas
-const cartasEmbaralhadas = embaralharCartas(cartas);
+criaCartao(
+    '07', 
+    'Mestre mandou...', 
+    'Bater palmas com os cotovelos.',
+    'elbow.png',
+    ''
+)
 
-// Chama a função criaCartao para cada carta embaralhada
-let contador = 1;
-cartasEmbaralhadas.forEach(carta => {
-    criaCartao(carta.personagem, carta.imagem, contador++);
-});
+criaCartao(
+    '08', 
+    'Mestre mandou...', 
+    'Fingir que a câmera travou com os seus olhos fechados.',
+    'smiling.png',
+    ''
+)
+
+criaCartao(
+    '09', 
+    'Mestre mandou...', 
+    'Fingir que está lutando Box.',
+    'boxing.png',
+    ''
+)
+
+criaCartao(
+    '10', 
+    'Mestre mandou...', 
+    'Mostrar a língua.',
+    'tongue-out.png',
+    'fake'
+)
+
+criaCartao(
+    '11', 
+    'Mestre mandou...', 
+    'Fingir que está tocando bateria.',
+    'drum-set.png',
+    ''
+)
+
+criaCartao(
+    '12', 
+    'Mestre mandou...', 
+    'Finjir que está matando um mosquito.',
+    'man.png',
+    ''
+)
+
+criaCartao(
+    '13', 
+    'Mestre mandou...', 
+    'Fingir levar um grande susto.',
+    'fright.png',
+    ''
+)
+
+criaCartao(
+    '14', 
+    'Mestre mandou...', 
+    'Dar um grito.',
+    'scream.png',
+    'fake'
+)
+
+criaCartao(
+    '15', 
+    'Mestre mandou...', 
+    'Tremer como se estivesse levando um choque.',
+    'electric-shock.png',
+    ''
+)
+
+criaCartao(
+    '16', 
+    'Mestre mandou...', 
+    'Abrir o microfone e gritar a frase "Eu amo programar!"',
+    'love.png',
+    ''
+)
